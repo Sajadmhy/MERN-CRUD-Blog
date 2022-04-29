c365a833577b359d04f2379054b39ce547730069
Sajadmhy <sajad.mahyaei@gmail.com>
2022-04-29 10:37:05 -0400

    Add controllers and routes to the server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import blogPosts from "./routes/blogPosts.routes.js";
+
 dotenv.config();
 
 const app = express();
@@ -12,14 +14,20 @@ app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
+// remember to add this after cors
+app.use("/api/blogs", blogPosts);
+
 const DB_CONNECTION = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 6000;
 
 mongoose
-  .connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
-      console.log(`Server is running @ : http://localhost:${PORT}`)
+      console.log(`Server is running at: http://localhost:${PORT}`)
     )
   )
-  .catch((error) => console.error(error));
+  .catch((error) => console.log(error));
